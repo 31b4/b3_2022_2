cfb886f30aea975ca546d144123fa4a9174785cd
31b4 <75566095+31b4@users.noreply.github.com>
2022-10-12 18:39:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+var hossz=parseInt($("#hossz").val());
 $(document).ready(function() {
     $("#jatek").click(function(){
+        hossz=parseInt($("#hossz").val());
         jelenlegi_jatekos = true
         $("#keret").html("");
         var table =document.createElement("table");
         document.getElementById("keret").appendChild(table)
-        var hossz=parseInt($("#hossz").val());
         if (hossz%2==1) {
             alert("Csak páros oldalú lehet")
             return
         }
         table.id="table";
         table.style.border="2px solid";
+        console.log(hossz)
         for (let i = 0; i < hossz; i++) {
             var tr = document.createElement("tr");
             for (let j = 0; j < hossz; j++) {
@@ -19,17 +21,18 @@ $(document).ready(function() {
                 td.style.width="25px";
                 td.style.border="2px solid";
                 td.id=i+';'+j;
+                td.style.backgroundColor="darkgreen"
                 td.addEventListener("click",()=>{Lepes(i,j)});
-                PontSzamitas(hossz,i,j,td)
+                PontSzamitas(i,j,td)
                 tr.appendChild(td);
             }
             table.appendChild(tr);
         }
-        KezdoKororngok(hossz)
+        KezdoKororngok()
         
     })
 })
-function PontSzamitas(hossz,i,j,td){
+function PontSzamitas(i,j,td){
     var pont;
     if (i<(hossz/2)) {
         if (j<(hossz/2)) {
@@ -72,9 +75,8 @@ function PontSzamitas(hossz,i,j,td){
 }
 
 
-function KezdoKororngok(hossz){
+function KezdoKororngok(){
     var kord = Number(hossz/2-1)
-    console.log(kord+";"+Number(kord+1))
     document.getElementById(kord+";"+kord).style.backgroundColor="white"
     document.getElementById(kord+";"+Number(kord+1)).style.backgroundColor="black"
     document.getElementById(Number(kord+1)+";"+kord).style.backgroundColor="black"
@@ -84,12 +86,65 @@ function KezdoKororngok(hossz){
 }
 var jelenlegi_jatekos = true;//true=p1; false = p2
 function Lepes(i,j) {
-    if (jelenlegi_jatekos) {
-        document.getElementById(i+";"+j).style.backgroundColor="black"
-        jelenlegi_jatekos=!jelenlegi_jatekos
+    if(ValidLepes(i,j)){
+        if (jelenlegi_jatekos) {
+            document.getElementById(i+";"+j).style.backgroundColor="black"
+            jelenlegi_jatekos=!jelenlegi_jatekos
+        }
+        else{
+            document.getElementById(i+";"+j).style.backgroundColor="white"
+            jelenlegi_jatekos=!jelenlegi_jatekos
+        }
     }
     else{
-        document.getElementById(i+";"+j).style.backgroundColor="white"
-        jelenlegi_jatekos=!jelenlegi_jatekos
+        alert("nem lephetsz ide");
     }
+}
+function ValidLepes(i,j) {
+
+    if(document.getElementById(i+";"+j).style.backgroundColor=="darkgreen"){
+        Iranyok(-1,0,i,j)
+        Iranyok(-1,+1,i,j)
+        Iranyok(0,+1,i,j)
+        Iranyok(+1,+1,i,j)
+        Iranyok(+1,0,i,j)
+        Iranyok(+1,-1,i,j)
+        Iranyok(0,-1,i,j)
+        Iranyok(-1,-1,i,j)
+    }
+    else{
+        return false;
+    }
+    return true;
+}
+function Iranyok(x,y,i,j) {
+    var ellenfel_color=""
+    var sajat_color=""
+    if (jelenlegi_jatekos){
+        ellenfel_color="white"
+        sajat_color="black"
+    }
+    else{
+        ellenfel_color = "black"
+        sajat_color="white"
+    }
+    if (i+x>=0 && i+x<hossz && j+y>=0 && j+y<hossz) {
+        console.log((i+x)+";"+(j+y));
+        var szorzo = 1;
+        var lepesek =[]
+        while(i+(x*szorzo)>=0 && i+(x*szorzo)<hossz && j+(y*szorzo)>=0 && j+(y*szorzo)<hossz 
+                && (document.getElementById(Number(i+(x*szorzo))+";"+Number(j+(y*szorzo))).style.backgroundColor ==ellenfel_color 
+                || document.getElementById(Number(i+(x*szorzo))+";"+Number(j+(y*szorzo))).style.backgroundColor ==sajat_color)){
+            if(document.getElementById(Number(i+(x*szorzo))+";"+Number(j+(y*szorzo))).style.backgroundColor ==sajat_color){
+                for (let i = 0; i < lepesek.length; i++) {
+                    document.getElementById(lepesek[i]).style.backgroundColor=sajat_color;
+                }
+                break;
+            }
+            lepesek.push(Number(i+(x*szorzo))+";"+Number(j+(y*szorzo)))
+            szorzo+=1;
+            console.log(i+(x*szorzo))
+            console.log(j+(y*szorzo)>=0)
+        }
+    }   
 }
