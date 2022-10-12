6f18b74b41ab6217dfd1b4889a89f65e979fa6c7
31b4 <75566095+31b4@users.noreply.github.com>
2022-10-12 14:15:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
         var table =document.createElement("table");
         document.getElementById("keret").appendChild(table)
         var hossz=parseInt($("#hossz").val());
+        if (hossz%2==1) {
+            alert("Csak páros oldalú lehet")
+            return
+        }
         table.id="table";
         table.style.border="2px solid";
         for (let i = 0; i < hossz; i++) {
@@ -14,12 +18,14 @@ $(document).ready(function() {
                 td.style.width="25px";
                 td.style.border="2px solid";
                 td.id=i+';'+j;
+                td.addEventListener("click",()=>{Lepes(i,j)});
                 PontSzamitas(hossz,i,j,td)
-                console.log(td.style.backgroundColor)
                 tr.appendChild(td);
             }
             table.appendChild(tr);
         }
+        KezdoKororngok(hossz)
+        
     })
 })
 function PontSzamitas(hossz,i,j,td){
@@ -53,10 +59,36 @@ function PontSzamitas(hossz,i,j,td){
             } 
         }
     }
-    td.innerHTML = pont;
+    //td.innerHTML = pont;
     td.value = pont;
+    /*
     if(pont%2==0){
         td.style.backgroundColor="yellow"
         $(td).css("backgroundcolor","yellow")
+    }
+    */
+
+}
+
+
+function KezdoKororngok(hossz){
+    var kord = Number(hossz/2-1)
+    console.log(kord+";"+Number(kord+1))
+    document.getElementById(kord+";"+kord).style.backgroundColor="white"
+    document.getElementById(kord+";"+Number(kord+1)).style.backgroundColor="black"
+    document.getElementById(Number(kord+1)+";"+kord).style.backgroundColor="black"
+    document.getElementById(Number(kord+1)+";"+Number(kord+1)).style.backgroundColor="white"
+    
+    
+}
+var jelenlegi_jatekos = true;//true=p1; false = p2
+function Lepes(i,j) {
+    if (jelenlegi_jatekos) {
+        document.getElementById(i+";"+j).style.backgroundColor="black"
+        jelenlegi_jatekos=!jelenlegi_jatekos
+    }
+    else{
+        document.getElementById(i+";"+j).style.backgroundColor="white"
+        jelenlegi_jatekos=!jelenlegi_jatekos
     }
 }
